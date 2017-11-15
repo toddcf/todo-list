1c60e631d0a32319068571050ef2bd789b8a365e
Todd Croak-Falen <toddcf@gmail.com>
2017-11-14 18:53:05 -0800

    Added strikethrough.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var lis = document.querySelectorAll( "li" );
 
 for ( var i = 0; i < lis.length; i++ ) {
 	lis[ i ].addEventListener( "mouseover", function() {
-		this.style.color = "yellow";
+		this.classList.add( "hovering" );
 	});
 
 	lis[ i ].addEventListener( "mouseout", function() {
-		this.style.color = "black";
+		this.classList.remove( "hovering" );
 	});
 
 	lis[ i ].addEventListener( "click", function() {
